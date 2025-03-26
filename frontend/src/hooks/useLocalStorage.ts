a6fce7da6fcad346b3b5ca07b9ef886dd5b30b0c
Ibai Garrido <igarridosi23wg@ikzubirimanteo.com>
2025-03-26 10:37:26 +0100

    Deploy changes for netlify 4

--- frontend/src/hooks/useLocalStorage.ts
+++ frontend/src/hooks/useLocalStorage.ts
@@ -18,7 +18,7 @@ export function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T)
     }
   }
 
-  // State to store our value
+  // State to store our value.
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState<T>(readValue)
 
